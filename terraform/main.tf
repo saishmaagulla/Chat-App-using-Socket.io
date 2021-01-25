ee3e1a3708f0a350af9d0b6d274faff96d17273b
saishmaagulla <agullasaishma@gmail.com>
2021-01-25 12:54:19 +0530

    updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-	source = "/modules/vpc"
+	source = ".//modules/vpc"
 	cidr_block = var.cidr_block
 	env = var.env
 	availability_zones = var.availability_zones
--- terraform/modules/vpc/variables.tf
+++ terraform/modules/vpc/variables.tf
@@ -1,4 +1,4 @@
-varaible "cidr_block" {
+variable "cidr_block" {
 	type = string
 	default = "10.0.0.0/16"
 }
