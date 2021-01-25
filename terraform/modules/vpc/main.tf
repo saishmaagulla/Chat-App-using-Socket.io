b392304261b042910915c85ca63ddc3449b4de5d
saishmaagulla <agullasaishma@gmail.com>
2021-01-25 12:56:13 +0530

    modified

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -2,7 +2,7 @@ resource "aws_vpc" "test_vpc" {
 	cidr_block = var.cidr_block
 	instance_tenancy = "default"
 	enable_dns_hostnames = true
-	enabke_dns_support = true
+	enable_dns_support = true
 	tags = {
 	Name = var.env
 	}
